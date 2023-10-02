b707d559b1a4fed97c1429cd2c70d2b55ec6c393
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2023-10-02 16:14:25 +0530

    Improve UI Category.js

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import fakeProducts from '../db/products.json';
 import categories from '../db/categories.json';
@@ -8,21 +8,19 @@ import FilterCheckbox from '../components/FilterCheckbox';
 import Products from '../components/Products';
 
 function getComputedProducts(products, filters) {
-  // Clone the products array to avoid mutating the original data
-  const result = [...products];
+  // const result = products; would create a reference thus doesn't apply
+  // [...someArray] creates a new array from elements of someArray
+  let result = [...products];
 
   if (filters.delivery) {
-    // eslint-disable-next-line no-const-assign
     result = result.filter(p => p.delivery === true);
   }
 
   if (filters.inStock) {
-    // eslint-disable-next-line no-const-assign
     result = result.filter(p => p.inStock === true);
   }
 
   if (filters.expensive) {
-    // eslint-disable-next-line no-const-assign
     result = result.filter(p => p.price > 100);
   }
 
@@ -54,43 +52,43 @@ const Category = ({ category }) => {
   );
 
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={3} className="mt-4">
-          <div className="bg-light p-3 rounded">
-            <h2 className="h5 mb-4">Filters</h2>
-            <FilterCheckbox
-              id="delivery"
-              name="delivery"
-              checked={filter.delivery}
-              onChange={onCheckboxChange}
-              label="Delivery"
-            />
-            <FilterCheckbox
-              id="inStock"
-              name="inStock"
-              checked={filter.inStock}
-              onChange={onCheckboxChange}
-              label="In stock only"
-            />
-            <FilterCheckbox
-              id="expensive"
-              name="expensive"
-              checked={filter.expensive}
-              onChange={onCheckboxChange}
-              label="Expensive (100+ USD)"
-            />
-            <div className="mt-4">
-              <strong>Results:</strong> {filteredProducts.length} out of {products.length}
-            </div>
+    <Row>
+      <Col xs={12} md={3} className="mt-4">
+        <div className="bg-light p-3 rounded">
+          <h2 className="h5 mb-4">Filters</h2>
+          <FilterCheckbox
+            id="delivery"
+            name="delivery"
+            checked={filter.delivery}
+            onChange={onCheckboxChange}
+            label="Delivery"
+          />
+          <FilterCheckbox
+            id="inStock"
+            name="inStock"
+            checked={filter.inStock}
+            onChange={onCheckboxChange}
+            label="In stock only"
+          />
+          <FilterCheckbox
+            id="expensive"
+            name="expensive"
+            checked={filter.expensive}
+            onChange={onCheckboxChange}
+            label="Expensive (100+ USD)"
+          />
+          <div className="mt-4">
+          <strong>Results:</strong> {filteredProducts.length} out of {products.length}
           </div>
-        </Col>
-        <Col xs={12} md={9} className="mt-4">
-          <h1 className="h3">{category.name}</h1>
+        </div>
+      </Col>
+      <Col xs={12} md={9} className="mt-4">
+        <h1 className="h3">{category.name}</h1>
+        <div>
           <Products products={filteredProducts} />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
